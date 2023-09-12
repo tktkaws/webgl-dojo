46ad970bbf69a00be092d6bdae902dbd3c4e7c99
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-09-12 20:42:50 +0900

    glslファイルを読み込む

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import glsl from "vite-plugin-glsl";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      glsl({
+        watch: false,
+      }),
+    ],
+  },
+});
