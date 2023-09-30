4a6568604178fc4bb5d19270892a2ada1440d01d
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-09-30 14:56:56 +0900

    Add:noise04

---
+++ src/pages/noise/noise04/fragment.glsl
@@ -0,0 +1,18 @@
+precision mediump float;
+
+#pragma glslify:noise2=require(glsl-noise/simplex/2d);
+#pragma glslify:noise3=require(glsl-noise/simplex/3d);
+
+varying vec2 vUv;
+uniform float uTick;
+
+void main(){
+    
+    // 通常のノイズ
+    float n=noise2(vUv);
+    
+    //   // 波のように揺れる
+    n=noise2(vec2(vUv.x*100.-sin(vUv.y+uTick/100.),vUv.y*10.-sin(vUv.x+uTick/100.)));
+    
+    gl_FragColor=vec4(n,0.,.5,1.);
+}
