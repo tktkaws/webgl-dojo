83ff3a4dad21855dc3ac5ce5811f3e764279c421
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-09-30 14:58:26 +0900

    Add: glslify glsl-noise vite-plugin-glslify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
-import glsl from "vite-plugin-glsl";
+// import glsl from "vite-plugin-glsl";
+import glslify from "vite-plugin-glslify";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [
-      glsl({
-        watch: false,
-      }),
+      glslify(),
+      // glsl({
+      //   watch: false,
+      // }),
     ],
   },
 });
