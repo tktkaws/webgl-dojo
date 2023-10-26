48ad12c286ee31c5d697dddd1f204c9705bd305b
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-10-26 21:27:54 +0900

    Add: vite-plugin-glslify / glsl-easings / gsap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
 import { defineConfig } from "astro/config";
-// import glsl from "vite-plugin-glsl";
 import glslify from "vite-plugin-glslify";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [
-      glslify(),
-      // glsl({
-      //   watch: false,
-      // }),
-    ],
+    plugins: [glslify()],
   },
 });
