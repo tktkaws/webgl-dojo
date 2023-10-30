6711f08a38dde5415b4dae717cc02bf21cc406b9
takayuki-takahashi-dic <ttkaws15@gmail.com>
2023-10-30 17:16:32 +0900

    Fix:ファイルパスの変更

--- src/pages/render_target/02_shader/fragment.glsl
+++ src/pages/render_target/02_shader/fragment.glsl
@@ -3,7 +3,6 @@ uniform sampler2D tDiffuse;
 
 void main(){
     vec4 texColor=texture(tDiffuse,vUv);
-    // gl_FragColor=texColor;
+    gl_FragColor=texColor;
     gl_FragColor=vec4(0.,texColor.g,0.,1.);
-    
 }
